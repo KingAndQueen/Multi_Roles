f02196dad3d3dcd0be8cffc463123529dc4d9ff8
liyw17 <liyw17@lenovo.com>
2017-10-30 14:26:19 +0800

    feat: add tensorboard

--- Multi_Roles_Data.py
+++ Multi_Roles_Data.py
@@ -1,98 +1,111 @@
 import os
 import pdb
-import numpy as np
 import pickle
+
+import numpy as np
+
+
 class Vocab():
-    def __init__(self,word2vec=None,embed_size=0):
-        self.word2idx={'<eos>':0,'<go>':1,'<pad>':2,'<unk>':3}
-        self.idx2word={0:'<eos>',1:'<go>',2:'<pad>',3:'<unk>'}
-        self.embed_size=embed_size
-    def add_vocab(self,words):
+    def __init__(self, word2vec=None, embed_size=0):
+        self.word2idx = {'<eos>': 0, '<go>': 1, '<pad>': 2, '<unk>': 3}
+        self.idx2word = {0: '<eos>', 1: '<go>', 2: '<pad>', 3: '<unk>'}
+        self.embed_size = embed_size
+
+    def add_vocab(self, words):
         if isinstance(words, (list, np.ndarray)):
             for word in words:
                 if word not in self.word2idx:
-                    index=len(self.word2idx)
-                    self.word2idx[word]=index
-                    self.idx2word[index]=word
+                    index = len(self.word2idx)
+                    self.word2idx[word] = index
+                    self.idx2word[index] = word
         else:
             if words not in self.word2idx:
                 index = len(self.word2idx)
                 self.word2idx[words] = index
                 self.idx2word[index] = words
-    def word_to_index(self,word):
+
+    def word_to_index(self, word):
         self.add_vocab(word)
         return self.word2idx[word]
-    def index_to_word(self,index):
+
+    def index_to_word(self, index):
         if index in self.idx2word:
             return self.idx2word[index]
         else:
             return '<unk>'
+
     @property
     def vocab_size(self):
         return len(self.idx2word)
 
-def read_file(data_path,vocabulary,sentence_size,roles_number):
-    f=open(data_path,'r')
-    scene={}
+
+def read_file(data_path, vocabulary, sentence_size, roles_number):
+    f = open(data_path, 'r')
+    scene = {}
     scenes = []
-    last_speaker=''
-    name_list=[]
+    last_speaker = ''
+    name_list = []
     for lines in f:
-        if len(lines)>2:
-            name=lines[:lines.index(':')]
-            #name_id=vocabulary.word_to_index(name)#for word in name.split()]
-            sentence=lines[lines.index(':')+1:lines.index(':')+1+sentence_size-1] #sub 1 for eos
-            sentence_id=[vocabulary.word_to_index(word)for word in sentence.split()]
+        if len(lines) > 2:
+            name = lines[:lines.index(':')]
+            # name_id=vocabulary.word_to_index(name)#for word in name.split()]
+            sentence = lines[lines.index(':') + 1:lines.index(':') + 1 + sentence_size - 1]  # sub 1 for eos
+            sentence_id = [vocabulary.word_to_index(word) for word in sentence.split()]
             sentence_id.append(vocabulary.word_to_index('<eos>'))
-            padding_len=max(sentence_size-len(sentence_id),0)
+            padding_len = max(sentence_size - len(sentence_id), 0)
             for i in range(padding_len):
                 sentence_id.append(vocabulary.word_to_index('<pad>'))
-            scene[name]=sentence_id
-            last_speaker=name
-            if name in ['Monica','Joey','Chandler','Phoebe', 'Rachel','Ross']:
+            scene[name] = sentence_id
+            last_speaker = name
+            if name in ['Monica', 'Joey', 'Chandler', 'Phoebe', 'Rachel', 'Ross']:
                 name_list.append(vocabulary.word_to_index(name))
         else:
             if last_speaker not in scene:
                 continue
-            ans=scene[last_speaker]
+            ans = scene[last_speaker]
             ans.pop()
-            ans.insert(0,vocabulary.word_to_index('<go>'))
-            scene['ans']=ans
-            weight=[]
+            ans.insert(0, vocabulary.word_to_index('<go>'))
+            scene['ans'] = ans
+            weight = []
             for id in scene[last_speaker]:
-                if id==vocabulary.word_to_index('<pad>'):
+                if id == vocabulary.word_to_index('<pad>'):
                     weight.append(0.0)
                 else:
                     weight.append(1.0)
-            scene['weight']=weight
-            name_pad=roles_number-len(name_list)
-            if name_pad<0:pdb.set_trace()
-            for i in range(name_pad):name_list.append(vocabulary.word_to_index('<pad>'))
-            scene['name']=name_list
-            scene[last_speaker]=sentence_size*[vocabulary.word_to_index('<pad>')]
+            scene['weight'] = weight
+            name_pad = roles_number - len(name_list)
+            if name_pad < 0: pdb.set_trace()
+            for i in range(name_pad): name_list.append(vocabulary.word_to_index('<pad>'))
+            scene['name'] = name_list
+            scene[last_speaker] = sentence_size * [vocabulary.word_to_index('<pad>')]
             scenes.append(scene)
-            scene={}
-            name_list=[]
+            scene = {}
+            name_list = []
     f.close()
     return scenes
 
-def get_data(data_path,vocabulary,sentence_size,roles_number):
+
+def get_data(data_path, vocabulary, sentence_size, roles_number):
     train_data_path = os.path.join(data_path, 'Train.txt')
     test_data_path = os.path.join(data_path, 'Test.txt')
-    return read_file(train_data_path,vocabulary,sentence_size,roles_number),read_file(test_data_path,vocabulary,sentence_size,roles_number)
+    return read_file(train_data_path, vocabulary, sentence_size, roles_number), read_file(test_data_path, vocabulary,
+                                                                                          sentence_size, roles_number)
 
-def store_vocab(vocab,data_path):
-    data_path=data_path+'vocab.pkl'
-    f=open(data_path,'w')
-    pickle.dump(vocab,f)
+
+def store_vocab(vocab, data_path):
+    data_path = data_path + 'vocab.pkl'
+    f = open(data_path, 'wb')
+    pickle.dump(vocab, f)
     f.close()
+
+
 def get_vocab(data_path):
     data_path = data_path + 'vocab.pkl'
     if os.path.exists(data_path):
-        f=open(data_path,'r')
-        vocab=pickle.load(f)
+        f = open(data_path, 'r')
+        vocab = pickle.load(f)
         f.close()
     else:
         print('<<<<<<< vocab is not exist >>>>>>>')
-        vocab=None
-    return  vocab
+        vocab = None
+    return vocab
--- Multi_Roles_main.py
+++ Multi_Roles_main.py
@@ -1,98 +1,109 @@
-import tensorflow as tf
-import numpy as np
-import Multi_Roles_Model
-import Multi_Roles_Data
 import os
 import random
-from sklearn import model_selection
-#set parameters of model
-flags=tf.app.flags
-flags.DEFINE_string('model_type','test','whether model initial from checkpoints')
-flags.DEFINE_string('data_dir','data/','data path for model')
-flags.DEFINE_string('checkpoints_dir','checkpoints/','path for save checkpoints')
-flags.DEFINE_string('summary_path','./summary','path of summary for tensorboard')
-flags.DEFINE_string('device_type','gpu','device for computing')
 
-flags.DEFINE_integer('layers',1,'levels of rnn or cnn')
-flags.DEFINE_integer('neurons',50,'neuron number of one level')
-flags.DEFINE_integer('batch_size',128, 'batch_size')
-flags.DEFINE_integer('roles_number',6,'number of roles in the data')
-flags.DEFINE_integer('epoch',10,'training times' )
-flags.DEFINE_integer('check_epoch',5,'training times' )
-flags.DEFINE_integer('sentence_size',20,'length of sentence')
-flags.DEFINE_float('interpose',0.5,'value for gru gate to decide interpose')
-flags.DEFINE_float('learn_rate',0.5,'value for gru gate to decide interpose')
-flags.DEFINE_float("learning_rate_decay_factor", 0.99,'if loss not decrease, multiple the lr with factor')
-flags.DEFINE_float("max_grad_norm",5,'Clip gradients to this norm')
-config=flags.FLAGS
+import numpy as np
+import tensorflow as tf
+from sklearn import model_selection
+
+import Multi_Roles_Data
+import Multi_Roles_Model
+
+# set parameters of model
+flags = tf.app.flags
+flags.DEFINE_string('model_type', 'train', 'whether model initial from checkpoints')
+flags.DEFINE_string('data_dir', 'data/', 'data path for model')
+flags.DEFINE_string('checkpoints_dir', 'checkpoints/', 'path for save checkpoints')
+flags.DEFINE_string('summary_path', './summary', 'path of summary for tensorboard')
+flags.DEFINE_string('device_type', 'gpu', 'device for computing')
+
+flags.DEFINE_integer('layers', 1, 'levels of rnn or cnn')
+flags.DEFINE_integer('neurons', 50, 'neuron number of one level')
+flags.DEFINE_integer('batch_size', 128, 'batch_size')
+flags.DEFINE_integer('roles_number', 6, 'number of roles in the data')
+flags.DEFINE_integer('epoch', 2, 'training times')
+flags.DEFINE_integer('check_epoch', 1, 'training times')
+flags.DEFINE_integer('sentence_size', 20, 'length of sentence')
+flags.DEFINE_float('interpose', 0.5, 'value for gru gate to decide interpose')
+flags.DEFINE_float('learn_rate', 0.01, 'value for gru gate to decide interpose')
+flags.DEFINE_float("learning_rate_decay_factor", 0.01, 'if loss not decrease, multiple the lr with factor')
+flags.DEFINE_float("max_grad_norm", 5, 'Clip gradients to this norm')
+config = flags.FLAGS
 
 
 def show_result(seq, vocab):
     if isinstance(seq, (list, np.ndarray)):
         words = []
         for idx in seq:
-            if isinstance(idx,(list,np.ndarray)):
-                show_result(idx,vocab)
+            if isinstance(idx, (list, np.ndarray)):
+                show_result(idx, vocab)
             else:
                 if vocab.index_to_word(idx) == '<eos>': break
                 words.append(vocab.index_to_word(idx))
-        print (words)
+        print(words)
     if isinstance(seq, (str, int)):
-        print (vocab.idx_to_word(seq))
+        print(vocab.idx_to_word(seq))
 
-def data_process(config,vocabulary=None):
-#read data from file and normalized
-    if vocabulary==None:
-        vocabulary=Multi_Roles_Data.Vocab()
-    train_data,test_data=Multi_Roles_Data.get_data(config.data_dir,vocabulary,config.sentence_size,config.roles_number)
-    print('data processed,vocab size:',vocabulary.vocab_size)
-    Multi_Roles_Data.store_vocab(vocabulary,config.data_dir)
-    return train_data,test_data,vocabulary
 
-#training model
-def train_model(sess,model,train_data):
-    train_data,eval_data=model_selection.train_test_split(train_data,test_size=0.2)
-    current_step=1
+def data_process(config, vocabulary=None):
+    # read data from file and normalized
+    if vocabulary == None:
+        vocabulary = Multi_Roles_Data.Vocab()
+    train_data, test_data = Multi_Roles_Data.get_data(config.data_dir, vocabulary, config.sentence_size,
+                                                      config.roles_number)
+    print('data processed,vocab size:', vocabulary.vocab_size)
+    Multi_Roles_Data.store_vocab(vocabulary, config.data_dir)
+    return train_data, test_data, vocabulary
+
+
+# training model
+def train_model(sess, model, train_data):
+    train_data, eval_data = model_selection.train_test_split(train_data, test_size=0.2)
+    current_step = 1
     data_input_train = model.get_batch(train_data)
-    data_input_eval=model.get_batch(eval_data)
-    train_summary_writer = tf.summary.FileWriter(config.summary_path, sess.graph)
+    data_input_eval = model.get_batch(eval_data)
+    train_summary_writer = tf.summary.FileWriter(config.summary_path + "/train", sess.graph)
+    evaluation_summary_writer = tf.summary.FileWriter(config.summary_path + "/evaluation", sess.graph)
     print('training....')
     checkpoint_path = os.path.join(config.checkpoints_dir, 'MultiRoles.ckpt')
-    while current_step<config.epoch:
-      #  print ('current_step:',current_step)
-        previous_losses=[]
+    while current_step < config.epoch:
+        print('current_step:', current_step)
+        previous_losses = []
         for i in range(len(data_input_train)):
-            loss,_,summary=model.step(sess,data_input_train[i])
+            loss, _, summary = model.step(sess, data_input_train[i])
             previous_losses.append(loss)
-        if current_step%config.check_epoch==0:
+        if current_step % config.check_epoch == 0:
             if len(previous_losses) > 2 and loss > max(previous_losses[-3:]):
                 sess.run(model.learning_rate_decay_op)
-            print ('current_step:', current_step)
-            print ('training total loss:',loss)
+            print('current_step:', current_step)
+            print('training total loss:', loss)
             train_summary_writer.add_summary(summary, current_step)
-            loss_eval=0.0
-            for i in range(len(data_input_eval)):
-                loss,_,_=model.step(sess,data_input_eval[i])
-                loss_eval+=loss
-            print ('evaluation total loss:',loss_eval/len(data_input_eval))
-            print ('saving current step %d checkpoints....' % current_step)
+            train_summary_writer.flush()
+
+            eval_data = random.choice(data_input_eval)
+            loss_eval, _, evaluation_summary = model.step(sess, eval_data)
+            evaluation_summary_writer.add_summary(evaluation_summary, current_step)
+            evaluation_summary_writer.flush()
+            print('evaluation total loss:', loss_eval)
+            # tf.summary.scalar("'evaluation_total_loss", loss_eval / len(data_input_eval))
+            print('saving current step %d checkpoints....' % current_step)
             model.saver.save(sess, checkpoint_path, global_step=current_step)
 
-        current_step+=1
+        current_step += 1
 
-def test_model(sess,model,test_data,vocab):
-    data_input_test=model.get_batch(test_data)
-    loss_test=0.0
-    predicts=[]
-    for batch_id,data_test in enumerate(data_input_test):
-        loss,predict,_=model.step(sess,data_test,step_type='test')
-        loss_test+=loss
-        print('labels: Id:',batch_id)
-        show_result(data_test.get('answer'),vocab)
+
+def test_model(sess, model, test_data, vocab):
+    data_input_test = model.get_batch(test_data)
+    loss_test = 0.0
+    predicts = []
+    for batch_id, data_test in enumerate(data_input_test):
+        loss, predict, _ = model.step(sess, data_test, step_type='test')
+        loss_test += loss
+        print('labels: Id:', batch_id)
+        show_result(data_test.get('answer'), vocab)
         predicts.append(predict)
-        print ('predicts: Id:',batch_id)
+        print('predicts: Id:', batch_id)
         show_result(predict, vocab)
-    print('test total loss:',loss_test/len(data_input_test))
+    print('test total loss:', loss_test / len(data_input_test))
 
 
     # data_input_test = model.get_batch(test_data)
@@ -101,13 +112,14 @@ def test_model(sess,model,test_data,vocab):
     # show_result(test_sample.get('answer'),vocab)
     # show_result(predict,vocab)
 
-#testing model
+
+# testing model
 def main(_):
-    train_data,test_data,vocab=data_process(config)
+    train_data, test_data, vocab = data_process(config)
     # initiall model from new parameters or checkpoints
     sess = tf.Session()
 
-    if config.model_type == 'train' :
+    if config.model_type == 'train':
         print('establish the model...')
         model = Multi_Roles_Model.MuliRolesModel(config, vocab)
         ckpt = tf.train.get_checkpoint_state(config.checkpoints_dir)
@@ -117,8 +129,8 @@ def main(_):
         else:
             print("Created model with fresh parameters....")
             sess.run(tf.global_variables_initializer())
-        train_model(sess, model,train_data)
-        test_model(sess,model,test_data,vocab)
+        train_model(sess, model, train_data)
+        test_model(sess, model, test_data, vocab)
     if config.model_type == 'test':
         print('establish the model...')
         # config.batch_size = 1
@@ -126,7 +138,7 @@ def main(_):
         print('Reload model from checkpoints.....')
         ckpt = tf.train.get_checkpoint_state(config.checkpoints_dir)
         model.saver.restore(sess, ckpt.model_checkpoint_path)
-        test_model(sess,model,test_data,vocab)
+        test_model(sess, model, test_data, vocab)
 
 
 if __name__ == "__main__":
