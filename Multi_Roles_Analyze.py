efa07c3dadac674a9d3d47485200941122b3dd6d
young <qichuanyang@hotmail.com>
2017-12-07 15:20:36 +0800

    fix bugs

--- Multi_Roles_Analyze.py
+++ Multi_Roles_Analyze.py
@@ -20,8 +20,8 @@ class Multi_Roles_Analyze():
                 sents_id=data_test[name]
                 sents=[vocab.index_to_word(id) for id in sents_id[key]]
                 pdb.set_trace()
-                if '<pad>' in sents:
-                    sents = sents.pop(sents.index('<pad>'))
+                while '<pad>' in sents:
+                    sents.pop(sents.index('<pad>'))
                 if '<eos>' in sents:
                     sents=sents[:sents.index('<eos>')]
                 print(name, sents)
